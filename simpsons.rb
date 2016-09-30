06bfdcf62b3e951118610a65c883f2db3b1c7c55
Katie Black <black.katherine.g@gmail.com>
2016-09-30 00:23:36 -0400

    Update simpsons.rb

--- simpsons.rb
+++ simpsons.rb
@@ -5,6 +5,8 @@ def list(names)
   array = []
   if names.length == 1
   	 names[0][:name]
+  elsif names.length == 0
+        return ""
   elsif names.length == 2
   	 names[0][:name] + " & " + names[1][:name]
   else
